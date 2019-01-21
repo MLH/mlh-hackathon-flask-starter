f75db637948cbb2cf89b208ece7e724bd0f8ab87
nlaz <nikolazaris@utexas.edu>
2019-01-21 15:11:09 -0500

    Remove unused authentication routes

--- starter/views/auth.py
+++ starter/views/auth.py
@@ -18,58 +18,6 @@ github_oauth_url = 'https://github.com/login/oauth/authorize?client_id={}&client
     GITHUB_CLIENT_SECRET,
 )
 
-@blueprint.route('/register', methods=('GET', 'POST'))
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        error = None
-
-        if not username:
-            error = 'Username is required.'
-        elif not password:
-            error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
-            error = 'User {} is already registered.'.format(username)
-
-        if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
-            return redirect(url_for('auth.login'))
-
-        flash(error)
-
-    return render_template('auth/register.html')
-
-@blueprint.route('/login', methods=('GET', 'POST'))
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
-
-        if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password'
-
-        if error is None:
-            session.clear()
-            session['user_id'] = user['id']
-            return redirect(url_for('public.index'))
-
-        flash(error)
-
-    return render_template('auth/login.html')
-
 @blueprint.route('/github/login')
 def githubLogin():
     return redirect(github_oauth_url)
