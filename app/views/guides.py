8d4a451b645b2cf1393be2620b6b2efd22a27b98
Niko Lazaris <nikolazaris@gmail.com>
2019-02-15 16:43:43 -0500

    Update searching and finding pages

--- app/views/guides.py
+++ app/views/guides.py
@@ -1,49 +1,41 @@
 # -*- coding: utf-8 -*-
 from flask import redirect, render_template, request
-from flask import Blueprint, flash, url_for, session
+from flask import g, Blueprint, flash, url_for, session
 
 from app.views.auth import login_required
 from app.services.github import GitHub
 
-blueprint = Blueprint('guides', __name__)
+blueprint = Blueprint('guides', __name__, url_prefix='/guides')
 
 @blueprint.route('/fetching')
 def fetching():
     if not 'access_token' in session:
-        flash('This page needs an authenticated user. Please sign in with your GitHub account.', 'warning')
         return render_template('guides/fetching.html')
 
     github = GitHub(access_token=session['access_token'])
+    results = github.get('/user/starred')
 
-    starred_repos = github.get('/user/starred')
-    return render_template('guides/fetching.html', repos=starred_repos[:5])
+    return render_template('guides/fetching.html', repos=results[:5])
 
 @blueprint.route('/searching')
 def searching():
-    if not 'access_token' in session:
-        flash('This page needs an authenticated user. Please sign in with your GitHub account.', 'warning')
-        return render_template('guides/searching.html')
-
-    github = GitHub(access_token=session['access_token'])
-
-    starred_repos = github.get('/user/starred')
-    return render_template('guides/searching.html', repos=starred_repos[:5])
+    return render_template('guides/searching.html')
 
 @blueprint.route('/search')
 def search():
     search = request.args.get('query')
 
     if search is None or search == '':
-        flash('Please include a repo name you want to search for.', 'danger')
+        flash('Please include a valid search query.', 'danger')
         return redirect(url_for('guides.searching'))
     if not 'access_token' in session:
-        flash('Please sign in with your GitHub account.', 'danger')
+        flash('This example needs an authenticated user to make the request. Please sign in with your GitHub account.', 'danger')
         return redirect(url_for('guides.searching'))
 
     github = GitHub(access_token=session['access_token'])
     repos = github.get('/search/repositories', { 'q': search } )
 
-    return render_template('guides/searching.html', repos=repos['items'])
+    return render_template('guides/searching.html', repos=repos['items'][:5])
 
 @blueprint.route('/star', methods=['POST'])
 def star():
